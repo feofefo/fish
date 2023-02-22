8d75ae4b7a19679ba05880cfbbd8f83133ee112d
wobushilaowu <laowudeemail@163.com>
2023-02-22 13:40:00 +0800

    first commit

---
+++ wx/pages/index/index.vue
@@ -0,0 +1,18 @@
+<template>
+	<view class="container">
+		{{count}}
+	</view>
+</template>
+
+<script setup>
+import { ref } from "vue";
+	const count = ref(10)
+</script>
+
+<style>
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+</style>
